ebacf873ec830a2a3bfd5406f7f8f858ffebb98a
jonyappleseed <undergrad@protonmail.com>
2020-03-26 18:52:41 -0600

    php/header/general design

--- design/nav.php
+++ design/nav.php
@@ -1,22 +1,15 @@
-!!COMMON_TAGS!!
 <div class="sidenav">
-  <button class="dropdown-btn">creative
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-container">
-    <a href="./creative/poetry.html">poetry</a>
-  </div>
   <button class="dropdown-btn">geology
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="./geology/hall_ranch.html">hall ranch</a>
+    <a href="pages/geology/hall_ranch.html">hall ranch</a>
   </div>
   <button class="dropdown-btn">urban mapping
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="./urban_mapping/cycling_denver.html">cycling in denver</a>
+    <a href="pages/urban_mapping/cycling_denver.html">cycling in denver</a>
   </div>
   <a href="#">return to top</>
 </div>
